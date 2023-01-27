f580e2ab55ef22baa761078c21a8c8053b84d6ed
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-01-27 11:36:21 +0000

    Exercise2

--- Exercise 2.py
+++ Exercise 2.py
@@ -5,18 +5,18 @@ Created on Wed Jan 25 11:38:39 2023
 
 @author: henryhutchings
 """
+a = [1, 2, 3]
+b = [4, 5, 6]
 
-a = [1,2,3]
-b = [4,5,6]
-dot = 0 
+
 N = len(a)
-"def function(a,b):"
-    
-for i in range(N):
-        dot = a(i)*b(i)
-        print(dot)
-        "return dot "
-    
-"dot += function(a,b)"
+def dotproduct(a,b):
+    dot = 0
+    for i in range(N):
+        dot += a[i]*b[i]
+    return dot 
+   
+print(dotproduct(a, b))    
+
 
     
