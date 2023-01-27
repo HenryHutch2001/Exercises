cb3a55968c3283b1b5a8b5dd9011eacd23ab0dff
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-01-27 11:15:21 +0000

    Exercise 1 & 2

---
+++ Exercise 1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from math import pi
+
+N = 200000
+PiSum = 0
+
+
+def function(n):
+    a = ((4*n)+1)
+    b = ((4*n)+3)
+    
+    ab = a*b
+    
+    PiSum = 8/ab
+    return PiSum
+
+
+
+for i in range(N):
+    PiSum += function(i)
+    error = abs(pi - PiSum)
+    if error > 10^-7:
+        N = N+1
+    else:
+        pass
+print(error)
+print(N)
+print(PiSum)
+
+
+
+
+
+
+
---
+++ Exercise 2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 11:38:39 2023
+
+@author: henryhutchings
+"""
+
+a = [1,2,3]
+b = [4,5,6]
+dot = 0 
+N = len(a)
+"def function(a,b):"
+    
+for i in range(N):
+        dot = a(i)*b(i)
+        print(dot)
+        "return dot "
+    
+"dot += function(a,b)"
+
+    
