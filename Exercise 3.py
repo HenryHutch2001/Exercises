2ea8cb752025dcbeaf084a494d326c1be85b18d9
HenryHutch2001 <79278617+HenryHutch2001@users.noreply.github.com>
2023-01-30 13:16:48 +0000

    exercise 3b

--- Exercise 3.py
+++ Exercise 3.py
@@ -10,3 +10,12 @@ import numpy as np
 
 a = np.array([5, 4, 9, 2, 0, 4, 7, 2])
 print(a[-1])
+
+print(a[1:6])
+"Printing indexes 1-6 selects the second element in the array and prints each element up until the 7th, python indexing starts at 0"
+
+print(a[:-2])
+"Prints the every array element before the second to last element"
+
+print(a[::2])
+"Prints every other element in the array"
